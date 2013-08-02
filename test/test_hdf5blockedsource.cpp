fef7ba74cc8533a554a54f5ad8968a9ad9fc7c07
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-08-02 14:57:03 +0200

    extend HDF5BlockedSource test

--- test/test_hdf5blockedsource.cpp
+++ test/test_hdf5blockedsource.cpp
@@ -12,12 +12,18 @@ void test() {
     typedef typename HDF5BlockedSource<3, float>::V V;
    
     HDF5File f("test.h5", HDF5File::Open);
-    f.createDataset<3, float>("test", V(10,20,30), 0);
+    MultiArray<3, float> data(V(10,20,30));
+    FillRandom<float, float*>::fillRandom(data.data(), data.data()+data.size());
+    f.write<3, float>("test", data);
     f.close();
    
     HDF5BlockedSource<3, float> bs("test.h5", "test");
     shouldEqual(bs.shape(), V(10,20,30));
-    
+   
+    MultiArray<3, float> r(data.shape());
+    bool ret = bs.readBlock(Roi<3>(V(), data.shape()), r);
+    should(ret);
+    shouldEqualSequence(data.begin(), data.end(), r.begin());
 }
 }; /* struct HDF5BlockedSourceTest */
 
