b4da8eb91e0293398aafaf6ac4af92fd18fe2741
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-09-07 21:00:36 +0200

    travis: build in parallel

--- .travis_scripts/install_vigra.sh
+++ .travis_scripts/install_vigra.sh
@@ -15,4 +15,4 @@ export PATH=$VIRTUAL_ENV/bin:$PATH
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
 
 cmake -DDEPENDENCY_SEARCH_PREFIX=$VIRTUAL_ENV -DVIGRANUMPY_LIBRARIES="/usr/lib/libpython2.7.so;/usr/lib/libboost_python.so" ..
-make install
+make -j8 install
